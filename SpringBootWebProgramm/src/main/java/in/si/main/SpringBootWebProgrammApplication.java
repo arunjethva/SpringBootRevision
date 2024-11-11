000f0cd7bb34728277bfeda783a4f0b7a8443a5d
Arun Jethva <arunjethva383@gmail.com>
2024-11-11 09:02:28 +0530

    Add files via upload

---
+++ SpringBootWebProgramm/src/main/java/in/si/main/SpringBootWebProgrammApplication.java
@@ -0,0 +1,14 @@
+package in.si.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebProgrammApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootWebProgrammApplication.class, args);
+	}
+
+}
---
+++ SpringBootWebProgramm/src/main/java/in/si/main/controller/HelloController.java
@@ -0,0 +1,15 @@
+package in.si.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello arun ";
+	}
+}
